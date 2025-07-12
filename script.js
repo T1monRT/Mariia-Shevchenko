71fd8ecfd25b3fef8180d35479ccad6f6d7a49a0
T1monRT <timur.sheva.play@gmail.com>
2025-07-11 23:06:37 -0700

    big changes

--- script.js
+++ script.js
@@ -35,3 +35,29 @@ window.onscroll = function() {
   }
 };
 
+const swiper = new Swiper('.works-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+
